808784416fd5508847755f1c47db4b0600b04a91
Sheykin <sheykindms@gmail.com>
2022-03-20 00:57:31 +0300

    add Exercise5.fs

---
+++ FSexe/Exercise5.fs
@@ -0,0 +1,11 @@
+// 16.1
+let notDivisible =
+    function
+    | (number, divider) -> number % divider = 0
+
+// 16.2
+let prime n =
+    let rec check i =
+        i > n / 2 || (n % i <> 0 && check (i + 1))
+
+    check 2
