179808ab08905704c25b7c099e5701683612854d
Sheykin <sheykindms@gmail.com>
2022-03-26 13:04:22 +0300

    fix Exercise6.fs

--- FSexe/Exercise6.fs
+++ FSexe/Exercise6.fs
@@ -5,18 +5,16 @@ let rec pow =
     | (str, index) -> str + pow (str, index - 1)
 
 // 17.2
-let rec isIthChar =
+let isIthChar =
     function
-    | (_, index, _) when index < 0 -> false
-    | (str, _, _) when String.length str <= 0 -> false
+    | (str, index, _) when index < 0 || String.length str <= index -> false
     | (str, index, c) -> str.[index] = c
-
 
 // 17.3
 let rec occFromIth (str, index, c) =
     let rec loop =
         function
-        | (_, index, _, _) when index < 0 -> 0 
+        | (_, index, _, _) when index < 0 -> 0
         | (str, index, _, count) when String.length str <= index -> count
         | (str, index, c, count) when str.[index] = c -> loop (str, index + 1, c, count + 1)
         | (str, index, c, count) -> loop (str, index + 1, c, count)
@@ -27,7 +25,7 @@ let rec occFromIth (str, index, c) =
 //let a = pow ("hey", 1)
 //printf $"%s{a}"
 //
-//let b = isIthChar ("cbath", 2, 'a')
+//let b = isIthChar ("cbath", 6, 'a')
 //printf $"%b{b}"
 //
 //let c = occFromIth ("aaaaccca", 2, 'c')
