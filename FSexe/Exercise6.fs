d1c7cd98dd41aefb934bde67302cbbbb118d9bbb
Sheykin <sheykindms@gmail.com>
2022-03-26 12:07:53 +0300

    fix Exercise6.fs

--- FSexe/Exercise6.fs
+++ FSexe/Exercise6.fs
@@ -5,18 +5,29 @@ let rec pow =
     | (str, index) -> str + pow (str, index - 1)
 
 // 17.2
-let isIthChar (str: string) (index: int) (c: char) = str.Chars(index) = c
+let rec isIthChar =
+    function
+    | (_, index, _) when index < 0 -> false
+    | (str, _, _) when String.length str <= 0 -> false
+    | (str, index, c) -> (string str).[index] = c
 
 
 // 17.3
-let rec occFromIth (getStr: string) (c: char) =
-    let rec loop i count =
-        if i < getStr.Length then
-            if getStr.[i] = c then
-                loop (i + 1) (count + 1)
-            else
-                loop (i + 1) count
-        else
-            count
+let rec occFromIth (str, index, c) =
+    let rec loop =
+        function
+        | (str, index, _, count) when String.length str <= index -> count
+        | (str, index, c, count) when (string str).[index] = c -> loop (str, index + 1, c, count + 1)
+        | (str, index, c, count) -> loop (str, index + 1, c, count)
 
-    loop 0 0
+    loop (str, index, c, 0)
+
+
+//let a = pow ("hey", 2)
+//printf $"%s{a}"
+//
+//let b = isIthChar ("cbath", 2, 'a')
+//printf $"%b{b}"
+//
+//let c = occFromIth ("aaath", 0, 'a')
+//printf $"%d{c}"
