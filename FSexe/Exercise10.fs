afb1b76d3d15e1e2a5923e47e9b53760c642cf3b
Sheykin <sheykindms@gmail.com>
2022-04-05 10:47:14 +0300

    fix Exercise10.fs

--- FSexe/Exercise10.fs
+++ FSexe/Exercise10.fs
@@ -1,13 +1,13 @@
 type TimeOfDay = { hours: int; minutes: int; f: string }
 
 let (.>.) (x:TimeOfDay) (y:TimeOfDay) =
-    if x.f = "PM" && x.f <> y.f then x > y
-    elif y.f = "PM" && x.f <> y.f then y > x
-    elif x.hours * 60 + x.minutes > y.hours * 60 + y.minutes then x > y
-    else y > x
+    if x.f = "PM" && x.f <> y.f then true
+    elif y.f = "PM" && x.f <> y.f then false
+    elif x.hours * 60 + x.minutes > y.hours * 60 + y.minutes then true
+    else false
 
 
-//let greaterTime = {hours = 11; minutes = 40; f = "AM"}
-//let lesserTime = {hours = 10; minutes = 59; f = "AM"}
+//let greaterTime = {hours = 11; minutes = 59; f = "AM"}
+//let lesserTime = {hours = 00; minutes = 00; f = "AM"}
 //
 //printf $"{greaterTime .>. lesserTime}"
