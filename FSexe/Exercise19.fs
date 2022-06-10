90f384ca638f9a75ba9e521168bca82bf9c53780
Sheykin <sheykindms@gmail.com>
2022-06-10 18:45:48 +0300

    fix Exercise19.fs

--- FSexe/Exercise19.fs
+++ FSexe/Exercise19.fs
@@ -1,14 +1,17 @@
 // 48.4.1 Напишите две версии функции вычисления n-го числа Фибоначчи Fn.
 let rec fibo1 n n1 n2 =
-    if (n = 0) then 0
-    elif n <= 2 then 1
+    if n = 0 then 0
+    elif n = 1 then n1
     else fibo1 (n - 1) (n1 + n2) n1
+    
+//let i = fibo1 6 1 0
+//printf $"{i}"
 
 // 48.4.2 Напишите две версии функции вычисления n-го числа Фибоначчи Fn.
 let rec fibo2 n c =
-    if (n = 0) then
+    if n = 0 then
         c 0
-    elif (n = 1) then
+    elif n = 1 then
         c 1
     else
         (fibo2 (n - 1) c) + (fibo2 (n - 2) c)
