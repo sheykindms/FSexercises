5b4432eefec3de3a9d8a95b686687a7b08016b82
Sheykin <sheykindms@gmail.com>
2022-06-13 02:30:35 +0300

    add Exercise22.fs

---
+++ FSexe/Exercise22.fs
@@ -0,0 +1,17 @@
+type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
+
+let hd (s : 'a cell) : 'a =
+  match s with
+    Nil -> failwith "hd"
+  | Cons (x, _) -> x
+
+let tl (s : 'a cell) : Lazy<'a cell> =
+  match s with
+    Nil -> failwith "tl"
+  | Cons (_, g) -> g
+
+
+// 51.3 Напишите с помощью hd и tl функцию nth, которая возвращает n-й (n >= 0) вычисленный элемент бесконечного списка.
+let rec nth (s : 'a cell) (n : int) : 'a =
+    if n = 0 then hd s
+    else nth ((tl s).Force()) (n-1)
